49ccd609282b73390ec86a3f0d87071f8ca89eeb
Shim0ji <shimoji@hotmail.fr>
2024-03-29 08:55:10 +0100

    ArticleDAOimpl methodes

--- src/main/java/fr/eni/ecole/encheres/bll/ArticleService.java
+++ src/main/java/fr/eni/ecole/encheres/bll/ArticleService.java
@@ -1,12 +1,15 @@
 package fr.eni.ecole.encheres.bll;
 
+import java.security.Principal;
 import java.util.List;
 
+import fr.eni.ecole.encheres.bo.Adresse;
 import fr.eni.ecole.encheres.bo.ArticleAVendre;
-import fr.eni.ecole.encheres.exceptions.BusinessException;
 
 public interface ArticleService {
 	List<ArticleAVendre> charger();
 	
-	void creerArticle(ArticleAVendre newArticle);
+	void creerArticle(ArticleAVendre newArticle, Principal p);
+
+	Adresse getAdress(String pseudo);
 }
